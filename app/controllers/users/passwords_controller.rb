f60ec1de87bf5e33b60fbaa6000790b9340719dd
nagmo01 <bjworkshop01@gmail.com>
2024-01-13 16:24:35 +0900

    devise:install

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Users
+  class PasswordsController < Devise::PasswordsController
+    # GET /resource/password/new
+
+    # POST /resource/password
+
+    # GET /resource/password/edit?reset_password_token=abcdef
+
+    # PUT /resource/password
+
+    # protected
+
+    # The path used after sending reset password instructions
+  end
+end
