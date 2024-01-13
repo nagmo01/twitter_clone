a48eccd9322e3b126fc2f3e0a6815242c52f9adf
nagmo01 <bjworkshop01@gmail.com>
2024-01-13 22:34:46 +0900

    cashing true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "myapp_production"
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
