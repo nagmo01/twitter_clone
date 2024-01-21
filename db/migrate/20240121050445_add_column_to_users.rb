52b3d2e2d6a0cf988941dfaaa32ae478a9e67037
nagmo01 <bjworkshop01@gmail.com>
2024-01-21 23:47:10 +0900

    uid,provider追加。tel,birthdayNULL制約削除

---
+++ db/migrate/20240121050445_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
+    add_index :users, [:provider, :uid], unique: true
+  end
+
+end
---
+++ db/migrate/20240121142559_change_column_to_null.rb
@@ -0,0 +1,6 @@
+class ChangeColumnToNull < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :users, :tel, true
+    change_column_null :users, :birthday, true
+  end
+end
