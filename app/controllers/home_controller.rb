55f1ff94d1cb1c03c5194e93a5a32aabeefe7439
nagmo01 <bjworkshop01@gmail.com>
2024-02-07 00:44:33 +0900

    フォロー中のツイート一覧表示を実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
     @user = User.find(current_user.id)
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :desc).page(params[:page])
+    @follower_tweets = follower_tweets.order(created_at: :desc).page(params[:page])
   end
 
   def show
@@ -13,5 +14,14 @@ class HomeController < ApplicationController
     @tweets = @user.tweets.order(created_at: :desc).page(params[:page])
     @favorites = @user.favorites.order(created_at: :desc).page(params[:page])
     @retweets = @user.retweets.order(created_at: :desc).page(params[:page])
+  end
+
+  private
+
+  def follower_tweets
+    user = User.find(current_user.id)
+    @relationship = user.following_relationship
+    @relationship = @relationship.map(&:followed_id)
+    Tweet.where(user_id: @relationship)
   end
 end
