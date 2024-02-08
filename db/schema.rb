1e9ecdcc261e6ab16319706eff4b82f9b8dea9c2
nagmo01 <bjworkshop01@gmail.com>
2024-02-08 21:25:34 +0900

    通知機能モデルとテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_06_062027) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_07_161723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_06_062027) do
     t.integer "sender_id", null: false
     t.integer "receiver_id", null: false
     t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visitor_id", null: false
+    t.integer "visited_id", null: false
+    t.integer "tweet_id"
+    t.integer "reply_id"
+    t.string "action_type", default: "", null: false
+    t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
