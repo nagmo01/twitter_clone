39cb5e1a48973aee286291942acf73328850c986
nagmo01 <bjworkshop01@gmail.com>
2024-02-09 16:48:22 +0900

    通知の種類の判別に定数を利用

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,20 +2,19 @@
 
 class NotificationMailer < ApplicationMailer
   default from: 'railsecsite@gmail.com'
+  ACTION_TYPE_MAPPING = {
+    'favorite' => 'いいね',
+    'retweet' => 'リツイート',
+    'follow' => 'フォロー',
+    'reply' => 'リプライ'
+  }.freeze
 
   def notification_email(notification)
     @user = notification.visited
     @notification = notification
-    case notification.action_type
-    when 'favorite'
-      @action = 'いいね'
-    when 'retweet'
-      @action = 'リツイート'
-    when 'follow'
-      @action = 'フォロー'
-    when 'reply'
-      @action = 'リプライ'
-    end
+
+    @action = ACTION_TYPE_MAPPING[notification.action_type]
+
     @url = ENV['SITE_URL']
 
     mail(to: @user.email, subject: "新しい#{@action}が届きました。")
